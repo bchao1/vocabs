a63ad462f4b78a42fd8e5b5c50e562b50f797af9
Mckinsey666 <whereisbrianchao520@gmail.com>
2019-02-24 13:18:26 +0800

    add setup

--- lib/config.py
+++ lib/config.py
@@ -1,3 +1,3 @@
 # Please change PATH to the ABSOLUTE path to the local dictionary file.
-DICT_DIR = "/mnt/d/SourceCodes/Python/Vocab/local/"
-DICT_PATH = "/mnt/d/SourceCodes/Python/Vocab/local/dict"
+DICT_DIR = "./local"
+DICT_PATH = "./local/dict"
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+      name='vocab',
+      version='1.0',
+      description='A lightweight online dictionary integration to the command line.',
+      url='https://github.com/Mckinsey666/Vocab',
+      keywords = "cli english words dictionary vocabulary",
+      author='Mckinsey666',
+      license='MIT',
+      packages=find_packages(),
+    )
